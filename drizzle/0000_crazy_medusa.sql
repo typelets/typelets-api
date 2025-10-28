17c44908373e85d29190e7e8afdd16097b1a350d
Rui Costa <rui@batalabs.com>
2025-10-28 13:22:09 -0400

    fix: enable database migrations for CI tests

--- drizzle/0000_crazy_medusa.sql
+++ drizzle/0000_crazy_medusa.sql
@@ -1,6 +1,5 @@
 -- Current sql file was generated after introspecting the database
--- If you want to run this migration please uncomment this code before executing migrations
-/*
+-- Migration uncommented and ready to run
 CREATE TABLE "file_attachments" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"note_id" uuid,
@@ -62,4 +61,3 @@ ALTER TABLE "folders" ADD CONSTRAINT "folders_parent_id_folders_id_fk" FOREIGN K
 ALTER TABLE "notes" ADD CONSTRAINT "notes_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "notes" ADD CONSTRAINT "notes_folder_id_folders_id_fk" FOREIGN KEY ("folder_id") REFERENCES "public"."folders"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "idx_file_attachments_note_id" ON "file_attachments" USING btree ("note_id" uuid_ops);
-*/
